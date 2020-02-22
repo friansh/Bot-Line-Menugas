6a244c50604034eddb30d3f00a8fb74d21f691c6
Fikri Rida Pebriansyah <aku@fikrirp.com>
2020-02-23 05:44:10 +0700

    Hadeh.

--- load.php
+++ load.php
@@ -3,4 +3,4 @@
 require_once(__DIR__ . '/Library/LINEBotTiny.php');
 require_once(__DIR__ . '/Library/LINEMsgParams.php');
 require_once(__DIR__ . '/Library/Database.php');
-require_once(__DIR__ . '/Config/Variables.php');
+require_once(__DIR__ . '/Library/Variables.php');
